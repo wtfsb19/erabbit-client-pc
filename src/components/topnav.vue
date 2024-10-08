a307a417efdce1d7ebc8b87b523a8ec930914bfc
shaobing <wtfsb19@gmail.com>
2024-10-08 15:07:01 +0800

    顶部菜单栏完成

---
+++ src/components/topnav.vue
@@ -0,0 +1,77 @@
+<template>
+  <nav class="app-topnav">
+    <div class="container">
+      <ul>
+        <template v-if=profile.token>
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ profile.account }}</a></li>
+          <li><a href="javascript:;">退出登录</a></li>
+        </template>
+        <template v-else>
+          <li><a href="javascript:;">请先登录</a></li>
+          <li><a href="javascript:;">免费注册</a></li>
+        </template>
+        <li><a href="javascript:;">我的订单</a></li>
+        <li><a href="javascript:;">会员中心</a></li>
+        <li><a href="javascript:;">帮助中心</a></li>
+        <li><a href="javascript:;">关于我们</a></li>
+        <li><a href="javascript:;"><i class="iconfont icon-phone"></i>手机版</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script>
+import { useUserStore } from '@/store/userStore'
+import { computed } from 'vue'
+
+export default {
+  name: 'AppTopnav',
+  setup () {
+    // 获取用户信息用于切换菜单
+    const userStore = useUserStore()
+    // 1. 通过storeToRefs方法获取响应式数据
+    // const { userInfo: {profile} } = storeToRefs(userStore)
+    // 2. 通过computed方法获取响应式数据
+    // 2.通过compute
+    const profile = computed(() => userStore.userInfo.profile);
+    return {
+      profile
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+.app-topnav {
+  background: #333;
+
+  ul {
+    display: flex;
+    height: 53px;
+    justify-content: flex-end;
+    align-items: center;
+
+    li {
+      a {
+        padding: 0 15px;
+        color: #cdcdcd;
+        line-height: 1;
+        display: inline-block;
+
+        i {
+          font-size: 14px;
+          margin-right: 2px;
+        }
+
+        &:hover {
+          color: @xtxColor;
+        }
+      }
+
+      ~ li {
+        a {
+          border-left: 2px solid #666;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,12 +1,16 @@
 <script>
+import topnav from '@/components/topnav.vue'
 export default {
-  name: 'xtx-layout'
+  name: 'xtx-layout',
+  components: {
+    topnav
+  }
 }
 </script>
 
 <template>
   <div>
-    <nav>顶部通栏</nav>
+    <topnav></topnav>
     <header>头部</header>
     <main>
       <!-- 二级路由 -->
