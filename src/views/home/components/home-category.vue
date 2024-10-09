a0f3fc584d11f4d1d7ac763ffaf06f0af7dc9e4e
shaobing <wtfsb19@gmail.com>
2024-10-09 16:22:17 +0800

    左侧导航栏弹层

--- src/views/home/components/home-category.vue
+++ src/views/home/components/home-category.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='home-category'>
     <ul class="menu">
-      <li v-for="item in menuList" :key="item.id">
+      <li v-for="item in menuList" :key="item.id" @mouseenter="currentId = item.id">
         <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         <!-- 二级菜单(由于初始数据无children，直接遍历会报错) -->
         <template v-if="item.children">
@@ -14,12 +14,29 @@
         </template>
       </li>
     </ul>
+    <!-- 弹层 -->
+    <div class="layer">
+      <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <!-- 获取数据后进行渲染，且当有数据时才渲染 -->
+      <ul v-if="currentCategory && currentCategory.goods && currentCategory.goods.length">
+        <li v-for="item in currentCategory.goods" :key="item.id">
+          <RouterLink to="/">
+            <img :src="item.picture" alt="">
+            <div class="info">
+              <p class="name ellipsis-2">{{item.name}}</p>
+              <p class="desc ellipsis">{{item.desc}}</p>
+              <p class="price"><i>¥</i>{{item.price}}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import { useCategoryStore } from '@/store/category'
-import { computed, reactive } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
 export default {
   name: 'HomeCategory',
@@ -31,7 +48,10 @@ export default {
     const brand = reactive({
       id: 'brand',
       name: '品牌',
-      children: [{ id: 'brand-chilren', name: '品牌推荐' }]
+      children: [{
+        id: 'brand-chilren',
+        name: '品牌推荐'
+      }]
     })
     // 取出store中的数据
     const categoryStore = useCategoryStore()
@@ -41,17 +61,26 @@ export default {
           id: item.id,
           name: item.name,
           // 防止初始化没有children的时候调用slice函数报错
-          children: item.children && item.children.slice(0, 2)
+          children: item.children && item.children.slice(0, 2),
+          goods: item.goods
         }
       })
       list.push(brand)
       return list
     })
 
-    console.log(menuList.value)
+
+    // 获取当前分类ID
+    const currentId = ref(null)
+    // 根据ID获取对应的分类
+    const currentCategory = computed(() => {
+      return categoryStore.categoryList.find(item => item.id === currentId.value)
+})
 
     return {
-      menuList
+      currentId,
+      menuList,
+      currentCategory
     }
   }
 }
@@ -83,6 +112,94 @@ export default {
           font-size: 16px;
         }
       }
+    }
+  }
+
+  .layer {
+    width: 990px;
+    height: 500px;
+    background: rgba(255, 255, 255, 0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+
+        a {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+
+          &:hover {
+            background: #e3f9f4;
+          }
+
+          img {
+            width: 95px;
+            height: 95px;
+          }
+
+          .info {
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+
+            .desc {
+              color: #999;
+            }
+
+            .price {
+              font-size: 22px;
+              color: @priceColor;
+
+              i {
+                font-size: 16px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  &:hover {
+    .layer {
+      display: block;
     }
   }
 }
